8ee2696683634e56538a9ee7a06354e0ab7d6d4d
Alexey Klochanov <Alexey.Klochanov@norbit.ru>
2023-11-22 17:49:55 +0300

    Начало. Функция EncryptCesar

---
+++ Cesar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Cesar
+{
+    internal class Program
+    {
+        private static string Alphavit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите сдвиг для шифра Цезаря: ");
+            var deltaStr = Console.ReadLine();
+            if (Int32.TryParse(deltaStr, out var delta))
+            {
+                var text = "тестовый Русский текст";
+                var encryptedText = EncryptCesar(text, delta);
+                Console.WriteLine(encryptedText);
+            }
+        }
+
+        private static string EncryptCesar(string text, int delta)
+        {
+            var lowercaseText = text.ToLower();
+            var module = Alphavit.Length;
+            var sb = new StringBuilder();
+            for (int i = 0; i < lowercaseText.Length; i++)
+            {
+                var oldIndex = Alphavit.IndexOf(lowercaseText[i]);
+                if (oldIndex == -1) continue;
+
+                var newIndex = (Alphavit.IndexOf(lowercaseText[i]) + delta) % module;
+                sb.Append(Alphavit[newIndex]);
+            }
+            return sb.ToString();
+        }
+    }
+}
